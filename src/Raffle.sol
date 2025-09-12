750ba04f004f5b90be04e54b5f3eca62136866c7
Shubham Nikam <nikamshubham224@gmail.com>
2025-09-12 16:27:51 +0530

    using require , which uses string for errMSG cost gass !

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,5 +2,29 @@
 
 pragma solidity ^0.8.19;
 
-contract Raffle{
+/**
+ *@title A Sample Raffle contract
+ *@author Shubham
+ *@notice This contrcat is for creating a simple raffle
+ *@dev Implements Chainlink VRFv2.5
+ **/
+contract Raffle {
+    //connot change this /cheap for gas
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        //having string here is not very gas efficient 
+        require(msg.value>= i_entranceFee,"Not enough ETH sent");
+    }
+
+    function pickWinner() public {}
+
+    /** Getter functions  */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
