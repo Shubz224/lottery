a5eba0a6416e414dd8bd96aec8a4cbe56f374fca
Shubham Nikam <nikamshubham224@gmail.com>
2025-09-12 20:53:05 +0530

    Event logging and Code Refactoring

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -11,10 +11,16 @@ pragma solidity ^0.8.19;
 
 contract Raffle {
     /**Custom Errors */
-    error SendMoreToEnterRaffle();
+    error Raffle__SendMoreToEnterRaffle();
 
     //connot change this /cheap for gas
     uint256 private immutable i_entranceFee;
+    //data structure to store all players enters the lottry contest
+    address payable[] private s_players;
+
+    /**Events */
+
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -24,9 +30,18 @@ contract Raffle {
         //having string here is not very gas efficient
         // require(msg.value>= i_entranceFee,"Not enough ETH sent");
         if (msg.value < i_entranceFee) {
-            revert SendMoreToEnterRaffle();
+            revert Raffle__SendMoreToEnterRaffle();
         }
+        s_players.push(payable(msg.sender));
+
+        // 1. Makes migration easier
+        // 2. Makes frontend "indexing" easier
+        emit RaffleEntered(msg.sender);
     }
+
+    // 1.Get a random number
+    // 2. Use random number to pick a player
+    // 3. Be automatically called
 
     function pickWinner() public {}
 
