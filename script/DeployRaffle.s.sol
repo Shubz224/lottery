2e153e1c5d9f91a0690d1023341177c9e7230c26
Shubham Nikam <nikamshubham224@gmail.com>
2025-09-14 15:11:46 +0530

    deployment script and config function for multiple chain compatibility

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
